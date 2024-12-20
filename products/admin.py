40d1ac28c3c2baf067a58667262c48cc597c7cae
Onkar Ijare <ijareonkar2184@gmail.com>
2024-12-20 21:43:49 +0530

    add new product

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from products.models import AddProduct
+# from account.models import User
 
 # Register your models here.
+
+
+class AddProductAdmin(admin.ModelAdmin):
+    search_fields = ['product_name', 'product_category', 'product_type', 'brand', 'user_email']
+    list_display = ['product_name', 'price', 'brand', 'user_email']
+    # readonly_fields = ['user_email', 'user_phone'] 
+
+    fieldsets = (
+        ('Seller Info', {'fields': ('email', 'phone')}),
+        ('Product Info', {'fields': ('product_name', 'product_image', 'product_video', 'gender', 'product_category', 'product_type', 'description', 'price', 'color', 'brand', 'discount', 'discounted_price', 'stock', 'available_stock', 'sizes', 'shipping_price')}),
+    )
+
+    def user_email(self, obj):
+        return obj.user.email
+
+    def user_phone(self, obj):
+        return obj.user.phone
+
+admin.site.register(AddProduct, AddProductAdmin)
