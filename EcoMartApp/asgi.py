f0a197b2ef9e261ecdcdc46223180a30d9a6387b
Onkar Ijare <ijareonkar2184@gmail.com>
2025-01-16 20:53:42 +0530

    EcoMartApp

--- EcoMartApp/asgi.py
+++ EcoMartApp/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EcoMart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EcoMartApp.settings')
 
 application = get_asgi_application()
